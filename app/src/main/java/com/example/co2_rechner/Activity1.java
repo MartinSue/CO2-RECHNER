5a86f8230f68e6314eb841725c796dc65818326d
Martin Sütterlin <52957520+MartinSue@users.noreply.github.com>
2019-10-09 13:44:13 +0200

    Initial commit

---
+++ app/src/main/java/com/example/co2_rechner/Activity1.java
@@ -0,0 +1,14 @@
+package com.example.co2_rechner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1);
+    }
+}
