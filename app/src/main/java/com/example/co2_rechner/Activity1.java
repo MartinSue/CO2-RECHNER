dfea9af686e5152a87495dbee99f2371e3a74448
janaleonh <jana.leonh@gmail.com>
2019-10-11 16:46:24 +0200

    Übergabe von Variablen aus Activity 1 an Activity 2

--- app/src/main/java/com/example/co2_rechner/Activity1.java
+++ app/src/main/java/com/example/co2_rechner/Activity1.java
@@ -1,18 +1,25 @@
 package com.example.co2_rechner;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class Activity1 extends Activity implements AdapterView.OnItemSelectedListener {
+public class Activity1 extends Activity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
 
+
+    //Variablen für EditText
+    protected EditText editText_name;
+    protected EditText getEditText_strecke;
     //Variablen für SeekBar
     protected SeekBar seekbar;
     protected TextView seekbar_textview;
@@ -20,6 +27,14 @@ public class Activity1 extends Activity implements AdapterView.OnItemSelectedLis
     protected int seekbar_max = 30;
     protected int seekbar_start = 0;
 
+    public static int seekValue = 0;
+
+
+    //Variable für Button
+    protected Button button_berechnung;
+
+    //Sonstige Variablen
+    String TEST;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +64,7 @@ public class Activity1 extends Activity implements AdapterView.OnItemSelectedLis
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 seekbar_value = seekbar.getProgress();
                 seekbar_textview.setText(seekbar_value + " Liter/100km");
+                seekValue=seekbar_value;
             }
 
             @Override
@@ -61,17 +77,40 @@ public class Activity1 extends Activity implements AdapterView.OnItemSelectedLis
 
             }
         });
+
+        //Button
+        button_berechnung = findViewById(R.id.button_berechnung);
+        button_berechnung.setOnClickListener(this);
+
+        //EditText
+        editText_name = findViewById(R.id.plaintext_name);
+        getEditText_strecke = findViewById(R.id.plaintext_strecke);
     }
+
+
 
     //Toast bei Klick auf Item
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String text = parent.getItemAtPosition(position).toString();
-        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+        TEST = parent.getItemAtPosition(position).toString();
+
+
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
+    public void onClick(View view) {
+
+        Intent intent = new Intent(this, Activity2.class);
+        intent.putExtra("nameUebergabe", ""+editText_name.getText());
+        intent.putExtra("artUebergabe", ""+TEST);
+        intent.putExtra("verbrauchUebergabe", ""+seekbar_value);
+        intent.putExtra("streckeUebergabe", ""+getEditText_strecke.getText());
+        Toast.makeText(getApplicationContext(),TEST,Toast.LENGTH_SHORT).show();
+        startActivity(intent);
+
+        }
 }
--- app/src/main/java/com/example/co2_rechner/Activity2.java
+++ app/src/main/java/com/example/co2_rechner/Activity2.java
@@ -2,13 +2,36 @@ package com.example.co2_rechner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Activity2 extends AppCompatActivity {
+    TextView verbrauchAnzeige;
+    TextView kraftstoffAnzeige;
+    TextView nameAnzeige;
+    TextView streckeAnzeige;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        verbrauchAnzeige = findViewById(R.id.textView_verbrauchAnzeige);
+        streckeAnzeige = findViewById(R.id.textView_streckeAnzeige);
+        kraftstoffAnzeige = findViewById(R.id.textView_kraftstoffartAnzeige);
+        nameAnzeige = findViewById(R.id.textView_nameAnzeige);
+
+        Intent intent= getIntent();
+        nameAnzeige.setText("Name: " +intent.getStringExtra("nameUebergabe"));
+        kraftstoffAnzeige.setText("Kraftstoffart: " +intent.getStringExtra("kraftstoffUebergabe"));
+        verbrauchAnzeige.setText("Kraftstoffverbrauch: " +intent.getStringExtra("verbrauchUebergabe")+ " Liter auf 100 km");
+        streckeAnzeige.setText("Strecke: " +intent.getStringExtra("streckeUebergabe")+ " Kilometer");
+
+
+
     }
+
 }
