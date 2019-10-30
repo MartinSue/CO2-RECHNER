df11e9500262df619bc32266550d39d855c4ca1f
janaleonh <jana.leonh@gmail.com>
2019-10-30 15:19:37 +0100

    Bild

--- app/src/main/java/com/example/co2_rechner/Activity3.java
+++ app/src/main/java/com/example/co2_rechner/Activity3.java
@@ -2,15 +2,59 @@ package com.example.co2_rechner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Activity3 extends AppCompatActivity {
+public class Activity3 extends AppCompatActivity implements View.OnClickListener {
 
+    Button button_formelIntent;
+    Button button_baumIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_3);
+        button_baumIntent = findViewById(R.id.button_WebseiteBaum);
+        button_formelIntent =findViewById(R.id.button_WebseiteFormel);
+
+        button_baumIntent.setOnClickListener(this);
+        button_formelIntent.setOnClickListener(this);
+
 
         getSupportActionBar().hide();
+    }
+
+
+    protected Intent createIntentFormel(){
+        Uri httpUri = Uri.parse("https://www.deutsche-handwerks-zeitung.de/kraftstoffverbrauch-in-co2-ausstoss-umrechnen/150/3097/57956");
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(httpUri);
+
+        return intent;
+    }
+
+    protected Intent createIntentBaum(){
+        Uri httpUri = Uri.parse("https://www.plant-for-the-planet.org/de/informieren/baeume-sind-genial-2");
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(httpUri);
+
+        return intent;
+
+    }
+    @Override
+    public void onClick(View v) {
+
+        Intent intent = null;
+        if(v.getId()== R.id.button_WebseiteBaum){
+        intent = createIntentBaum();
+        }else{
+            intent = createIntentBaum();
+        }
+
+        startActivity(intent);
     }
 }
