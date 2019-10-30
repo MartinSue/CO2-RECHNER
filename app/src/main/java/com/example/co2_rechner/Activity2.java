3acafc7da699715aa57495544fd8b75b0bbe804f
janaleonh <jana.leonh@gmail.com>
2019-10-30 23:04:09 +0100

    Überprüfung ob Gramm oder Kilogramm in Activity 1 verlagert

--- app/src/main/java/com/example/co2_rechner/Activity2.java
+++ app/src/main/java/com/example/co2_rechner/Activity2.java
@@ -20,6 +20,7 @@ public class Activity2 extends AppCompatActivity implements View.OnClickListener
     Button baumAnzeige;
     String anzahlTree;
     String ErgebnisAbsolut;
+    boolean flagFuerKilo;
 
 
 
@@ -51,12 +52,13 @@ public class Activity2 extends AppCompatActivity implements View.OnClickListener
         streckeAnzeige.setText(intent.getStringExtra("streckeUebergabe") + getString(R.string.label_textview_kilometer));
         ergebnisSpezifischAnzeige.setText(intent.getStringExtra("ergebnisSpezifisch")+getText(R.string.label_textview_gramm));
         ErgebnisAbsolut = intent.getStringExtra("ergebnisAbsolut");
+        flagFuerKilo= intent.getBooleanExtra("flagFuerKilo",false);
 
-        // Überprüfung ob Ausgabe als Gramm oder Kilogramm
-        if (Double.valueOf(intent.getStringExtra("ergebnisAbsolut")) > 1000){
-            double erg = Double.valueOf(intent.getStringExtra("ergebnisAbsolut"));
-            erg = erg/1000;
-            ergebnisAbsolutAnzeige.setText("" + erg + getText(R.string.label_textview_kilogramm));
+
+
+
+        if (flagFuerKilo ==true){
+            ergebnisAbsolutAnzeige.setText("" + intent.getStringExtra("ergebnisAbsolut")+ getText(R.string.label_textview_kilogramm));
         }else{
             ergebnisAbsolutAnzeige.setText(intent.getStringExtra("ergebnisAbsolut")+ getText(R.string.label_textview_gramm));
         }
